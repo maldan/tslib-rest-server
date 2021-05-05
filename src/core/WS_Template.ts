ee8c11d1a59d0ff637c2a7d450ca889145633f9b
Roman <blackwanted@yandex.ru>
2021-05-05 16:17:06 +0300

    Added -- ContentType config -- EJS Template Engine

--- src/core/WS_Template.ts
+++ src/core/WS_Template.ts
@@ -2,6 +2,6 @@ import EJS from 'ejs';
 
 export class WS_Template {
   static async file(path: string, data?: EJS.Data, options?: EJS.Options): Promise<string> {
-    return await EJS.renderFile(path, data, options);
+    return await EJS.renderFile(path, data, options); //
   }
 }
