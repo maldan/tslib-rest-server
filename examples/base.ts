8928dc3dc85be8b060a964b890cb0a6439206c34
Roman <blackwanted@yandex.ru>
2021-05-05 16:40:06 +0300

    Fix -- Shit

--- examples/base.ts
+++ examples/base.ts
@@ -4,4 +4,4 @@ import { MainApi } from './MainApi';
 const ws = new WebServer();
 // Init web server
 const web = new WebServer([new WS_Router('api', [MainApi]), new WS_Router('', [], ['./build'])]);
-ws.listen(3100);
+ws.listen(3100); //
