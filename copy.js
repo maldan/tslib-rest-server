651a9cd3749af222fe6895ae12e3e1c846087ddd
Roman <blackwanted@yandex.ru>
2021-06-04 12:36:48 +0300

    Fix -- Fuck

--- copy.js
+++ copy.js
@@ -1,5 +1,8 @@
 let fs = require('fs');
 let file = fs.readFileSync('./dist/doc/Container.js', 'utf-8');
 let pattern = fs.readFileSync('./src/doc/pattern.ejs', 'utf-8');
-file = file.replace('`%PATTERN%`', `JSON.parse(${JSON.stringify(pattern)})`);
+file = file.replace(
+  '`%PATTERN%`',
+  `Buffer.from("${Buffer.from(pattern, 'utf-8').toString('base64')}", "base64").toString('utf-8')`,
+);
 fs.writeFileSync('./dist/doc/Container.js', file);
